aca0a8ff7283cb374aafb063bfcede4c0dea981c
Sam-Aum <psaum01@gmail.com>
2022-03-10 17:52:17 -0500

    authentication

--- echo/settings.py
+++ echo/settings.py
@@ -128,3 +128,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'main_app/static')]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
--- main_app/models.py
+++ main_app/models.py
@@ -1,15 +1,18 @@
 from django.db import models
-# from django.
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Ekko(models.Model):
 
     # name = models.CharField(max_length=100)
-    user = models.CharField(max_length=100, default='sam-aum')
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     ekko = models.TextField(max_length=500)
     source = models.CharField(max_length=250, default='anonymous')
     verified_ekko = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+   
 
     def __str__(self):
         return self.ekko
